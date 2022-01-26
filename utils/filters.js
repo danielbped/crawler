8bc1bac61141a47bf499460e251e403b1149db06
Daniel Batista <danielbped@gmail.com>
2022-01-26 11:44:33 -0300

    Remove caracteres especiais dos nomes e atualiza README

--- utils/filters.js
+++ utils/filters.js
@@ -7,7 +7,7 @@ const {
 } = require('./Regex');
 
 const filterData = ({ data }) => data.match(CANDIDATE_REGEX);
-const filterName = (data) => data[0].match(NAME_REGEX)[0].split(' ').slice(1).join(' ').slice(0, -1).toUpperCase();
+const filterName = (data) => data[0].match(NAME_REGEX)[0].split(' ').slice(1).join(' ').slice(0, -1).toUpperCase().replace(SPECIAL_CHARACTERES_REGEX, '');
 const filterScore = (data) => data[0].match(SCORE_REGEX)[0].split(' ').slice(1)[0];
 
 const filterCpf = (data) => data.match(CPF_REGEX).map((cpf) => cpf.split('').splice(1).join(''));
